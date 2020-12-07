b0c7a2e22d67d266d6c08df35427516575e5215d
MOONYAN <c8763sasuke@ntut.org.tw>
2020-12-07 12:43:50 +0800

    scheduling update task

--- src/human/human.module.ts
+++ src/human/human.module.ts
@@ -2,10 +2,11 @@ import { DaoModule } from './../dao/dao.module';
 import { Module } from '@nestjs/common';
 import { HumanService } from './human.service';
 import { HumanController } from './human.controller';
+import { TaskService } from './task.service';
 
 @Module({
   imports: [DaoModule],
-  providers: [HumanService],
+  providers: [HumanService, TaskService],
   controllers: [HumanController]
 })
 export class HumanModule { }
---
+++ src/human/task.service.ts
@@ -0,0 +1,19 @@
+import { HumanService } from './human.service';
+import { Injectable, Logger } from '@nestjs/common';
+import { Cron, CronExpression } from '@nestjs/schedule';
+
+@Injectable()
+export class TaskService {
+
+    private readonly logger = new Logger(TaskService.name);
+
+    constructor(private humanService: HumanService) { }
+
+    @Cron(CronExpression.EVERY_10_SECONDS)
+    handleCron() {
+        this.logger.debug('Called every 10 seconds')
+        this.humanService.toRebornStage();
+        this.humanService.toDeadStage();
+        this.humanService.toGrowingStage();
+    }
+}
